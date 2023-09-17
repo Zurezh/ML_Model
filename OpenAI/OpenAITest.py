dfb26104c48bacb698334b8bf34d8c6a81c48709
Suresh Manem <sr.manem@gmail.com>
2023-09-17 18:15:04 +0530

    Advanced Chatbot Projects

--- OpenAI/OpenAITest.py
+++ OpenAI/OpenAITest.py
@@ -2,4 +2,4 @@ import pandas as pd
 import pandasai as pdai
 from pandasai.llm.openai import OpenAI
 
-llm = OpenAI(api_token=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)
+llm = OpenAI(api_token="Your API Token")
